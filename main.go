d119728ba9f06aa9c722c431d1a57b3eae76fa60
Ethan Xu <rainshanghsu@qq.com>
2019-09-12 11:15:43 +1000

    add page struct

--- main.go
+++ main.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/gocolly/colly"
 )
 
+type page struct {
+	no    int
+	items []item
+}
+
+func createPage(no int, pageURL string) *page {
+	c := colly.NewCollector()
+	items := make([]item, 0)
+
+	// big playing area
+	c.OnHTML(`div[class="podcasts container"]`, func(e *colly.HTMLElement) {
+		item := createItemFromBig(e)
+		go item.downloadTranscript()
+		items = append(items, *item)
+	})
+
+	// grid item
+	c.OnHTML(`div[class="grid__col large-1-2 xlarge-1-2 medium-1-2 small-no-pad"]`, func(e *colly.HTMLElement) {
+		item := createItemFromGrid(e)
+		go item.downloadTranscript()
+		items = append(items, *item)
+	})
+
+	c.Visit(pageURL)
+	return &page{
+		no,
+		items,
+	}
+}
+
 type item struct {
-	Title         string
-	URLTranscript string
+	title         string
+	urlTranscript string
 	transcript    string
-	URLMp3        string
+	urlMp3        string
 }
 
 func (it *item) downloadMp3() {
@@ -19,56 +50,57 @@ func (it *item) downloadMp3() {
 }
 
 func (it *item) downloadTranscript() {
+	c := colly.NewCollector()
+
 	// full transcript
-	// c.OnHTML(`div[id="transcripts-body"]`, func(e *colly.HTMLElement) {
-	// 	content := ""
-	// 	e.ForEach("p", func(_ int, el *colly.HTMLElement) {
-	// 		content += el.Text + "\n\n"
-	// 	})
-	// })
+	c.OnHTML(`div[id="transcripts-body"]`, func(e *colly.HTMLElement) {
+		content := ""
+		e.ForEach("p", func(_ int, el *colly.HTMLElement) {
+			content += el.Text + "\n\n"
+		})
+		it.transcript = content
+	})
+
+	c.Visit(it.urlTranscript)
 }
 
-func createItemFromBig(e *colly.HTMLElement) item {
+func createItemFromBig(e *colly.HTMLElement) *item {
 	title := e.ChildAttr(`div[class="podcasts-header podcasts-header--feature tooltip-outer"]`, "data-podcast-title")
 	transcript := e.ChildAttr(`a[class="t_meta underlined_text t_small"]`, "href")
 	mp3 := e.ChildAttr(`a[class="podcast-download"]`, "href")
-	return item{
-		Title:         title,
-		URLTranscript: transcript,
-		URLMp3:        mp3,
+	return &item{
+		title:         title,
+		urlTranscript: transcript,
+		urlMp3:        mp3,
 	}
 }
 
-func createItemFromGrid(e *colly.HTMLElement) item {
+func createItemFromGrid(e *colly.HTMLElement) *item {
 	title := e.Attr("data-podcast-title")
 	mp3 := e.ChildAttr(`a[data-tooltip-bounds-id="podcast-group"]`, "href")
 	transcript := e.ChildAttr(`a[class="t_meta underlined_text t_small"]`, "href")
-	return item{
-		Title:         title,
-		URLTranscript: transcript,
-		URLMp3:        mp3,
+	return &item{
+		title:         title,
+		urlTranscript: transcript,
+		urlMp3:        mp3,
 	}
 }
 
 func main() {
-	page := 1
-	items := make([]item, 0)
-	c := colly.NewCollector()
-
-	// big playing area
-	c.OnHTML(`div[class="podcasts container"]`, func(e *colly.HTMLElement) {
-		items = append(items, createItemFromBig(e))
-	})
-
-	// grid item
-	c.OnHTML(`div[class="grid__col large-1-2 xlarge-1-2 medium-1-2 small-no-pad"]`, func(e *colly.HTMLElement) {
-		items = append(items, createItemFromGrid(e))
-	})
+	pageNo := 1
+	pages := make([]page, 0)
+	waitGroup := sync.WaitGroup{}
 
 	start := time.Now()
-	for ; page < 3; page++ {
-		c.Visit(fmt.Sprintf("https://www.scientificamerican.com/podcast/60-second-science/?page=%d", page))
+	for ; pageNo < 3; pageNo++ {
+		waitGroup.Add(1)
+		go func(pageNo int) {
+			page := createPage(pageNo, fmt.Sprintf("https://www.scientificamerican.com/podcast/60-second-science/?page=%d", pageNo))
+			pages = append(pages, *page)
+			waitGroup.Done()
+		}(pageNo)
 	}
+	waitGroup.Wait()
 	end := time.Now()
 	fmt.Printf("totally used %d s", end.Sub(start)/time.Second)
 }
